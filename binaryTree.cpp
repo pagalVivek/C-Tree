125c1972ba60d019ae8924ab848a00b864d16b38
pagalVivek <naruto567uzumaki009@gmail.com>
2024-02-05 15:01:19 +0530

    initial!

---
+++ binaryTree.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+// using structure to define the tree
+struct Tree{
+    int data;
+    struct Tree* left;
+    struct Tree* right;
+};
+
+// using class to define the tree
+class cTree{
+    public:
+    int data;
+    cTree* left;
+    cTree* right;
+}
+
+
+
+
+
+int main(){
+
+    return 0;
+}

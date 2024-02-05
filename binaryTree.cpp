6a06bdb23f4ddaf2b997683ebae890eef34d8093
pagalVivek <naruto567uzumaki009@gmail.com>
2024-02-05 16:05:29 +0530

    dont't know

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 using namespace std;
 
+// binary tree traversals have 2 types
+// -> depth-first search
+// -> breadth-first searc
+
+// Depth-first search
+// it has 3 types
+// -> preorder || -> inorder || -> postorder
+// -> preorder = root - left child - right child
+// -> inorder = left child - root - right child
+// -> postorder = left child - right child - root
+
+// Breadth-first search
+//it has only one type
+// -> level order traversal
+// this works level by level and also left to right
+
+
+
 
 // using structure to define the tree
 struct Tree{
